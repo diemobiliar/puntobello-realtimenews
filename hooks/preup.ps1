0ad25b881f8274a7c5a4d8bdc63a6ddd7844a074
Mattias Bürgi [u112037] <mattias.buergi@mobi.ch>
2024-09-16 19:05:47 +0000

    reconnect az account if required.

--- hooks/preup.ps1
+++ hooks/preup.ps1
@@ -5,7 +5,6 @@ if (Test-Path /proc/1/cgroup) {
 }
 
 Invoke-Expression -Command $importPath/Deploy-SitesAndLists.ps1
-
 azd env set tenant_name $global:M365_TENANTNAME
 
 if (Test-Path "./spo/templates.json"){
@@ -27,4 +26,11 @@ if (Test-Path "./spo/templates.json"){
     $cnSite = Connect-PnPOnline -Url "https://$($global:M365_TENANTNAME).sharepoint.com/sites/$($sites[1])" @PnPCreds -ReturnConnection
     azd env set rtnews_en_sitepages_list_guid (Get-PnPList -Identity "SitePages" -Connection $cnSite).Id
     azd env set rtnews_en  $sites[1]
+}
+# sometimes the az cli connection is lost. in this case - connect again.
+$azAccount = az account show 2>$null
+if ($azAccount) {
+    Write-Host "Azure CLI is connected."
+} else {
+    az login
 }
