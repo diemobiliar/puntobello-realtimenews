c3ef71c74a6ddcf02dfe73a1052c1fbc9cfd9353
Mattias Bürgi [u112037] <mattias.buergi@mobi.ch>
2024-09-16 19:17:13 +0000

    count the target sites connect-pnponline not required here.

--- hooks/postup.ps1
+++ hooks/postup.ps1
@@ -8,11 +8,11 @@ $env:SPFX_URL_SOCKET = "$(azd env get-value app_name).azurewebsites.net"
 $env:SPFX_LIST_ID_REALTIMENEWSLIST = $(azd env get-value rtnews_list_guid)
 
 $target = ((Get-Content -Path "./spo/templates.json" | ConvertFrom-Json).templates | Where-Object { $_.templateName -eq "RealtimeNews.xml" })
-if ($target.Count -ne 1) {
+if ($target.targets.Count -ne 1) {
     Write-Error "The `$target variable should only have 1 element."
     exit 1
 }
-$cnSite = Connect-PnPOnline -Url "https://$($global:M365_TENANTNAME).sharepoint.com/sites/$($target.targets)" @global:PnPCreds -ReturnConnection
+# ich glaub den brauchts nicht?? $cnSite = Connect-PnPOnline -Url "https://$($global:M365_TENANTNAME).sharepoint.com/sites/$($target.targets)" @global:PnPCreds -ReturnConnection
 [xml]$xml = Get-Content -Path "$($target.relativePath)/$($target.templateName)"
 $env:SPFX_PATH_REALTIMENEWSLIST = $xml.Provisioning.Templates.ProvisioningTemplate.Lists.ListInstance.Url        
 $env:SPFX_TERMSTORE_CHANNEL_GUID = Ensure-TermSet -termSetPath "$((Get-Content ./spo/templates.json | ConvertFrom-Json).templates.termSets | Sort-Object -Unique)"
